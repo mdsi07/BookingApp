e1b006afdb1303987980d7ab56642c5721906a4a
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-07-05 20:39:43 +0530

    Delete User Details from Local Storage

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
+//const userList = document.querySelector('#users');
 
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
@@ -24,26 +24,30 @@ function onSubmit(e) {
   else
   {
     // Create new list item with user
-    const li = document.createElement('li');
+    //const li = document.createElement('li');
 
     // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    //li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
 
     // Add HTML
     // li.innerHTML = `<strong>${nameInput.value}</strong>: ${emailInput.value}`;
 
     // Append to ul
-    userList.appendChild(li);
+    //userList.appendChild(li);
 
     //Local Storage
     // localStorage.setItem('name:',nameInput.value);
     // localStorage.setItem('email:',emailInput.value);
 
+    // Storing input values
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
     // Creating Object
     let userDetails = {
-      name: nameInput.value,
-      email: emailInput.value
-    };
+      name,
+      email
+    }
 
     // convert object to String
     let userDetails_serialized = JSON.stringify(userDetails);
@@ -54,8 +58,37 @@ function onSubmit(e) {
     // if we want String to Objects then
     // let userDetails_deserialized = JSON.parse(localStorage.getItem('userDetails'));
 
+    showListofRegisteredUser(userDetails);
+
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
   }
+}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  Object.keys(localStorage).forEach(key => {
+      const user = JSON.parse(localStorage.getItem(key))
+      showListofRegisteredUser(user)
+  })
+})
+
+function showListofRegisteredUser(user) {
+  const parentNode = document.getElementById('users');
+  const createNewUserHtml = `<li id='${user.email}'>${user.name} - ${user.email}
+  <button onclick=deleteUser('${user.email}')>Delete</button> </li>`
+  console.log(createNewUserHtml)
+  parentNode.innerHTML +=  createNewUserHtml;
+  console.log(parentNode.innerHTML)
+}
+
+function deleteUser(email) {
+  localStorage.removeItem(email)
+  removeItemFromScreen(email)
+}
+
+function removeItemFromScreen(email) {
+  const parentNode = document.getElementById('users');
+  const elem = document.getElementById(email)
+  parentNode.removeChild(elem);
 }
