c2b1fdcd19bc0c4016ce72b7f3454ca683e9f27a
mdsi07 <137636526+mdsi07@users.noreply.github.com>
2023-07-07 20:32:12 +0530

    Edit Button Added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Put DOM elements into variables
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
+const numberInput = document.querySelector('#phoneNumber')
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 //const userList = document.querySelector('#users');
@@ -11,7 +12,7 @@ myForm.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
   
-  if(nameInput.value === '' || emailInput.value === '') 
+  if(nameInput.value === '' || emailInput.value === '' || numberInput.value === '') 
   {
     // alert('Please enter all fields');
     msg.classList.add('error');
@@ -41,11 +42,13 @@ function onSubmit(e) {
 
     // Storing input values
     const name = e.target.name.value;
+    const number = e.target.phoneNumber.value;
     const email = e.target.email.value;
 
     // Creating Object
     let userDetails = {
       name,
+      number,
       email
     }
 
@@ -62,6 +65,7 @@ function onSubmit(e) {
 
     // Clear fields
     nameInput.value = '';
+    numberInput.value = '';
     emailInput.value = '';
   }
 }
@@ -75,11 +79,15 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
 function showListofRegisteredUser(user) {
   const parentNode = document.getElementById('users');
-  const createNewUserHtml = `<li id='${user.email}'>${user.name} - ${user.email}
-  <button onclick=deleteUser('${user.email}')>Delete</button> </li>`
-  console.log(createNewUserHtml)
+  const createNewUserHtml = `<li id='${user.email}'>${user.name} - ${user.number} - ${user.email}
+  <button onclick=editUser('${user.email}') style="color:blue" >Edit</button>
+  <button onclick=deleteUser('${user.email}') style="color:red" >Delete</button> </li>`;
+  //console.log(createNewUserHtml)
+  
+  console.log('');
+
   parentNode.innerHTML +=  createNewUserHtml;
-  console.log(parentNode.innerHTML)
+  //console.log(parentNode.innerHTML)
 }
 
 function deleteUser(email) {
@@ -91,4 +99,39 @@ function removeItemFromScreen(email) {
   const parentNode = document.getElementById('users');
   const elem = document.getElementById(email)
   parentNode.removeChild(elem);
+}
+
+function editUser(email) {
+  
+  // in local storage values are in String so, we convert it into Object and store it into temporary variable
+  var temp1 = JSON.parse(localStorage.getItem(email))
+
+  // after getting values in object, we store that values into temporary variable
+  var temp2 = Object.values(temp1);
+
+  // now storing the respective value's to the respective variable's
+  var tempName = temp2[0];
+  var tempNumber = temp2[1];
+  var tempEmail = temp2[2];
+
+  // now assign a variable's for respective input field's
+  var nameSpan = document.getElementById('name');
+  var numberSpan = document.getElementById('phoneNumber');
+  var emailSpan = document.getElementById('email');
+
+  // now set the values(edited text values) to the input field
+  nameSpan.value = tempName;
+  numberSpan.value = tempNumber;
+  emailSpan.value = tempEmail;
+
+  // console log for checking
+  // console.log(nameSpan);
+  // console.log(numberSpan);
+  // console.log(emailSpan);
+  
+  // console.log('Name: [' + tempName + ']  Number: [' + tempNumber + ']  Email: [' + tempEmail + ']');
+
+  // remove data from local storage as well as list present in the screen(interface)
+  localStorage.removeItem(email)
+  removeItemFromScreen(email)
 }
